13bb781f543328f2a8dc8012471813c5fe69e5a0
linus <brianlinus1753@gmail.com>
2020-06-23 16:24:34 -0700

    Created the list data structure to model my data

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Buy Milk");
+        items.add("Go to the gym");
+        items.add("Have fun");
+
     }
 }
