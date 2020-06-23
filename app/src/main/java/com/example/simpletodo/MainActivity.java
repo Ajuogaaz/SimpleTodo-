a1928903e4b9cfba4e1fcdd406dcab6901966459
linus <brianlinus1753@gmail.com>
2020-06-23 16:27:59 -0700

    Created the different views by finding by id

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +15,20 @@ public class MainActivity extends AppCompatActivity {
     List<String> items;
 
     Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.addbutton);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvitems);
+
+
 
         items = new ArrayList<>();
         items.add("Buy Milk");
